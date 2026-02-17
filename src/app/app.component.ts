24c1b3f6e3698b965ad3508fadb1f992aa2e96be
valiyeffa <firuzeveliyeva2@gmail.com>
2026-02-17 16:30:30 +0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TODO, User } from './models/model';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CoursesComponent } from "./components/courses/courses.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, CoursesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
