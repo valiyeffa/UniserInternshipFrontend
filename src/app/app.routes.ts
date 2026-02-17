d62a08ea4652d09f3b0f090be7e606455be2faa0
valiyeffa <firuzeveliyeva2@gmail.com>
2026-02-17 17:36:10 +0400

    pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LayoutComponent } from './layout/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { StudentsComponent } from './pages/students/students.component';
 import { CoursesComponent } from './pages/courses/courses.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
     {
@@ -12,6 +13,7 @@ export const routes: Routes = [
             {path:'', component: HomeComponent},
             {path:'students', component: StudentsComponent},
             {path:'courses', component: CoursesComponent},
+            {path:'about', component: AboutComponent},
         ]
     }
 ];
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
