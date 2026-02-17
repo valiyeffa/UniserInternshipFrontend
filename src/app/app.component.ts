12082a552e76f923486bd0dbadc254d6c141dc69
valiyeffa <firuzeveliyeva2@gmail.com>
2026-02-17 17:07:25 +0400

    layout structure done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,15 @@
 import { Component } from '@angular/core';
-import { TODO, User } from './models/model';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { CoursesComponent } from "./components/courses/courses.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, CoursesComponent],
+  imports: [RouterOutlet ,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'my-app';
-  count: number = 0;
-
-  increase() {
-    this.count++;
-  }
-
-  decrease() {
-    this.count--;
-  }
-
-  reset() {
-    this.count = 0;
-  }
-  
-  users: User[] = [
-    {
-      id: 1,
-      name: 'Firuza',
-      email: 'firuza@gmail.com',
-      isActive: true
-    },
-    {
-      id: 2,
-      name: 'Ali',
-      email: 'ali@gmail.com',
-      isActive: false
-    },
-    {
-      id: 3,
-      name: 'Firuza 123',
-      email: 'firuza@gmail.com',
-      isActive: true
-    },
-  ];
-
-  // !============================TODO START================================
-
-  newTodo: string = '';
-  todos: TODO[] = [];
-
-  addTodo() {
-    this.todos.push({
-      id: this.todos.length + 1,
-      task: this.newTodo,
-      completed: false
-    })
-
-    this.newTodo = '';
-  }
-
-  doneTodo(todo: TODO) {
-    todo.completed = !todo.completed;
-  }
-
-  delTodo(todo: TODO) {
-    this.todos = this.todos.filter(i => i.id !== todo.id)
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentsComponent } from './pages/students/students.component';
+import { CoursesComponent } from './pages/courses/courses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: LayoutComponent,
+        children:[
+            {path:'', component: HomeComponent},
+            {path:'students', component: StudentsComponent},
+            {path:'courses', component: CoursesComponent},
+        ]
+    }
+];
