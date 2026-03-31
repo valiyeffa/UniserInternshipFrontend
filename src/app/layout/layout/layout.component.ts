2a036bd502b2060fb9696aedfe8c20da081ad3e4
valiyeffa <firuzeveliyeva2@gmail.com>
2026-03-31 17:12:12 +0400

    last edits

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "../footer/footer.component";
 import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './layout.component.html',
 })
 export class LayoutComponent {
