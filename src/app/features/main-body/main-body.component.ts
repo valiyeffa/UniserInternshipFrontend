d849659dddf8d3b2fd361dbff0205d92b7fe3874
valiyeffa <firuzeveliyeva2@gmail.com>
2026-04-03 15:19:10 +0400

    task done

--- src/app/features/main-body/main-body.component.ts
+++ src/app/features/main-body/main-body.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, RouterLinkActive, RouterLinkWithHref, RouterOutlet } from "@angular/router";
+import { ActivatedRoute, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { GlobalService } from '../../services/global.service';
 
 @Component({
   selector: 'app-main-body',
-  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive],
+  imports: [RouterLinkActive, RouterOutlet],
   templateUrl: './main-body.component.html',
 })
 export class MainBodyComponent {
