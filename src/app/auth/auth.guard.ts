38140b0957deda4cbe9fa21104644fb83485f327
valiyeffa <firuzeveliyeva2@gmail.com>
2026-04-17 15:38:19 +0400

    task done

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,11 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { jwtDecode } from 'jwt-decode';
+import { AuthService } from '../services/auth.service';
+import { catchError, map, of } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
+  const authService = inject(AuthService);
 
   const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
 
   if (!token) {
     return router.createUrlTree(['/login']);
@@ -17,9 +21,28 @@ export const authGuard: CanActivateFn = (route, state) => {
     const isExpired = decoded.exp * 1000 < Date.now();
 
     if (isExpired) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('refreshToken');
-      return router.createUrlTree(['/login']);
+      if (!refreshToken) {
+        localStorage.removeItem('token');
+        return router.createUrlTree(['/login']);
+      }
+
+      return authService.refreshToken(refreshToken).pipe(
+        map((res: any) => {
+          if (!res?.token) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('refreshToken');
+            return router.createUrlTree(['/login']);
+          }
+
+          localStorage.setItem('token', res.token);
+          return true;
+        }),
+        catchError(() => {
+          localStorage.removeItem('token');
+          localStorage.removeItem('refreshToken');
+          return of(router.createUrlTree(['/login']));
+        })
+      );
     }
 
     return true;
